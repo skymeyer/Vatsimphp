9f95c529ec06aaddb5340c7d7e6e6deaabea23b3
Jelle Vink <jelle.vink@gmail.com>
2018-11-30 14:08:09 +0000

    Apply fixes from StyleCI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Vatsimphp package
  *
- * Copyright 2013 - Jelle Vink <jelle.vink@gmail.com>
+ * Copyright 2018 - Jelle Vink <jelle.vink@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 require_once __DIR__.'/../vendor/autoload.php';
